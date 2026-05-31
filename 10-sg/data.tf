472a0fc4be0cdeff2c5600dfdc8feb2324e7bb25
manimohan <manimohan.t22@gmail.com>
2026-05-31 16:41:56 +0530

    updated

--- 10-sg/data.tf
+++ 10-sg/data.tf
@@ -2,7 +2,7 @@ data "aws_ssm_parameter" "vpc_id" {
   name = "/${var.project_name}/${var.environment}/vpc_id"
 }
 
-# # Fetch the EKS-managed node SG
-# data "aws_ssm_parameter" "eks_node_sg_id" {
-#   name = "/${var.project_name}/${var.environment}/eks_node_sg_id"
-# }
+# Fetch the EKS-managed node SG
+data "aws_ssm_parameter" "eks_node_sg_id" {
+  name = "/${var.project_name}/${var.environment}/eks_node_sg_id"
+}
